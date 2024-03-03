fe1651013a3904381faca083340e4301212ab7b4
Vladislav <vladislav.krasnik@baltinfocom.ru>
2024-03-03 17:48:15 +0300

    InitialCommit

---
+++ src/main/java/ru/unosoft/ProblemCenter/ProblemCenterApplication.java
@@ -0,0 +1,13 @@
+package ru.unosoft.ProblemCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemCenterApplication.class, args);
+	}
+
+}
